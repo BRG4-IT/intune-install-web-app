16b20ba7ad26399063c6fe3e000cda2293f52ebc
Michael Rundel <admin@brg4.at>
2021-10-22 17:26:31 +0200

    added script and update readme

---
+++ Install-Web-App.ps1
@@ -0,0 +1,104 @@
+﻿<#
+    .SYNOPSIS
+        Creates shortcuts to a web app on the desktop using the default browser.
+    .DESCRIPTION
+        Author:  Michael Rundel/John Seerden (https://www.srdn.io)
+        Version: 2.0
+
+        Creates shortcuts to a web app, using the default browser, on the Desktop.
+    .PARAMETER ShortcutName
+        Display Name of the shortcut.
+    .PARAMETER ShortcutUrl
+        URL associated with the shortcut.
+    .PARAMETER ShortcutIconUrl
+        Icon associated with the shortcut.
+        
+    .NOTES
+        This script can either run in SYSTEM (for all users) or USER (current user only) context.
+    .EXAMPLE
+        Install-Web-App.ps1 -ShortcutName "Wikipedia" -ShortCutUrl "https://simple.wikipedia.org/" -ShortcutIconUrl "https://raw.githubusercontent.com/BRG4-IT/intune-install-web-app/main/demo-icon/wikipedia.ico"
+        Install-Web-App.ps1 -ShortcutName "Wikipedia" -ShortCutUrl "https://simple.wikipedia.org/" -ShortcutIconUrl "https://raw.githubusercontent.com/BRG4-IT/intune-install-web-app/main/demo-icon/wikipedia.ico" -Uninstall
+#>
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$ShortcutName="Wikipedia",
+
+    [Parameter(Mandatory = $true)]
+    [string]$ShortcutUrl="https://simple.wikipedia.org/",
+
+    [Parameter(Mandatory = $false)]
+    [string]$ShortcutIconUrl="https://raw.githubusercontent.com/BRG4-IT/intune-install-web-app/main/demo-icon/wikipedia.ico",
+    
+    [Parameter(Mandatory = $false)]
+    [Switch]$Uninstall = $false
+
+)
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+
+$PathIcon = ""
+$PathLNK = ""
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+# Check if running in SYSTEM context (Shortcut applies for all users)
+if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    write-host "SYSTEM context"
+    if ($ShortcutIconUrl) {
+        $PathIcon = "$($env:ProgramData)\Icons\$ShortcutName.ico"
+    }
+    $PathLNK = "$([Environment]::GetFolderPath('CommonDesktopDirectory'))\$ShortcutName.lnk"
+}
+# Running in USER context (Shortcut only applies for the logged on user)
+else {
+    write-host "USER context"
+    if ($ShortcutIconUrl) {
+        $PathIcon = "$($env:AppData)\Icons\$ShortcutName.ico"
+    }
+    $PathLNK = "$([Environment]::GetFolderPath("Desktop"))\$ShortcutName.lnk"
+}
+
+
+if ($Uninstall) {
+    write-host "uninstalling webapp"
+    if ($ShortcutIconUrl) {
+        if (Test-Path "$PathIcon") {
+            Remove-Item "$PathIcon" -Force
+        }
+    }
+    if (Test-Path "$PathLNK") {
+        Remove-Item "$PathLNK" -Force
+    }
+}
+else {
+    write-host "installing webapp"
+    if ($ShortcutIconUrl) {
+        $baseDir = $PathIcon -replace "\\$ShortcutName.ico",""
+        if (-not (Test-Path "$baseDir")) {
+            New-Item -Path "$baseDir" -ItemType Directory
+        }
+
+        # Let's Uninstall the existing shortcut, in case we want to replace it with a newer one.
+        if (Test-Path "$PathIcon") {
+            Remove-Item "$PathIcon" -Force
+        }
+
+        # Download the shortcut
+        try {
+            (New-Object System.Net.WebClient).DownloadFile($ShortcutIconUrl, "$PathIcon")
+            # (New-Object System.Net.WebClient).DownloadFile("https://www.brg4.at/tools/ico/untis.ico", "$PathIcon")
+        }
+        catch [Net.WebException] {
+            # Write-Host $_.Exception.ToString()
+            Write-Host "Icon URL invalid"
+        }
+    }
+    $Shortcut = $WScriptShell.CreateShortcut("$PathLNK") 
+    $Shortcut.TargetPath = $ShortcutUrl
+    if (Test-Path "$PathIcon") {
+        $Shortcut.IconLocation = "$PathIcon"
+    }
+    else {
+        $Shortcut.IconLocation = "%SystemRoot%\system32\netshell.dll, 85"
+    }
+    $Shortcut.Save()
+}
--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# intune-install-web-app
-Powershell script do install Icons for WebApps on Windwos Desktop  with intune
+# Creating Desktop Shortcuts with intune (User & System context)
+
+`install-web-app` lets you depoly desktop shortcuts to machines and users in your organisation. You can use the precompiled [install-web-app.intunewin](./install-web-app.intunewin) or create your own from the powerscript script file (see below). 
+
+The `-ShortcutIconUrl` can be omitted. In this case an generic windows icon will be used. If you use the parameter The URL has to point to an valid [ICO (file format)](https://en.wikipedia.org/wiki/ICO_(file_format)) file.
+
+
+## Example: Create a Desktop shortcut to Wikipedia
+
+Follow the example to create a desktop shortcut to [Simple Wikipedia](https://simple.wikipedia.org/) for all users in system context.
+
+
+App-Typ auswählen: __Windows-App (Win32)__
+
+Upload: [install-web-app.intunewin](./install-web-app.intunewin)
+
+### App-Informationen (App Information):
+
+Name:
+
+```
+Simple Wikipedia Desktop Shortcut (AllUsers)
+```
+
+Beschreibung/Descripton
+
+```
+Create a desktop shortcut for Simple Wikipedia (https://simple.wikipedia.org/) for all users
+```
+
+Herausgeber/Publisher:
+
+```
+Wikipedia Fondation
+```
+
+### Programm 
+
+Installationsbefehl
+
+```
+PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "Install-Web-App.ps1" -ShortcutName "Wikipedia" -ShortCutUrl "https://simple.wikipedia.org/" -ShortcutIconUrl "https://raw.githubusercontent.com/BRG4-IT/intune-install-web-app/main/demo-icon/wikipedia.ico"
+```
+
+Deinstallationsbefehl
+
+```
+PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "Install-Web-App.ps1" -ShortcutName "Wikipedia" -ShortCutUrl "https://simple.wikipedia.org/" -ShortcutIconUrl "https://raw.githubusercontent.com/BRG4-IT/intune-install-web-app/main/demo-icon/wikipedia.ico" -Uninstall
+```
+
+Installationsverhalten/Context: __System__
+
+
+### Erkennungsregeln (Detection ruls):
+
+Regelformat (Rule type): __Erkennungsregeln manuell konfigurieren__
+
+Rule type/Regel Typ: File/Datei
+
+Path/Pfad:
+
+```
+%public%\Desktop\
+```
+
+File or Folder/Datei oder Ordner: 
+
+```
+Wikipedia.lnk
+```
+
+Detection method: __File or folder exists__
+
+
+## Convert script to .intunewin
+
+If you do not want to use the precompiled [install-web-app.intunewin](./install-web-app.intunewin) file, here is how to create it on your own:
+
+1. Clone/download this repository
+2. Download the [IntuneWinAppUtil](https://github.com/Microsoft/Microsoft-Win32-Content-Prep-Tool) Programm
+3. Move `IntuneWinAppUtil.exe` to the root of this repository
+4. Open a command prompt or a Powershell console
+5. Navigate to the root of this repository
+6. Execute the following commands:
+
+```
+.\IntuneWinAppUtil.exe -c .\ -s install-web-app.ps1 -o .\
+```
+
